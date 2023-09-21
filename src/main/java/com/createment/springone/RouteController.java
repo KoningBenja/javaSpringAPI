beb838b6c375f0466ff70816384ee5bdcac5bdd3
Edu <edudiezgarcia@hotmail.com>
2023-09-21 14:03:01 +0200

    commm

--- src/main/java/com/createment/springone/RouteController.java
+++ src/main/java/com/createment/springone/RouteController.java
@@ -113,6 +113,28 @@ public class RouteController {
         return "table";
     }
 
+    @GetMapping("/customer/last")
+    public String getLastCustomer(Model model) {
+        String sqlQuery = "SELECT * FROM customers ORDER BY customer_id DESC LIMIT 1";
+
+        TableData booksTableData = populateCustomersModel(sqlQuery);
+
+        model.addAttribute("data", booksTableData.data);
+        model.addAttribute("tableName", booksTableData.tableName);
+
+        return "table";
+    }
+
+    @GetMapping("/customers/pig")
+    public String getPigCustomers(Model model) {
+        String sqlQuery = "SELECT CONCAT('Piggy ', 'Pig '), first_name FROM customers";
+        TableData booksTableData = populateCustomersModel(sqlQuery);
+        model.addAttribute("data", booksTableData.data);
+        model.addAttribute("tableName", booksTableData.tableName);
+
+        return "table";
+    }
+
     @GetMapping("/books")
     public String index(Model model) {
         String sqlQuery = "SELECT * FROM books";
